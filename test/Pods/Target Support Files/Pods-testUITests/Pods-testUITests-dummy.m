f0fe560a3f70f5ed24bcd9f7dac0e60b90e45926
lyp1992 <2294582526@qq.com>
2018-12-17 16:49:04 +0800

    push test

---
+++ test/Pods/Target Support Files/Pods-testUITests/Pods-testUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testUITests : NSObject
+@end
+@implementation PodsDummy_Pods_testUITests
+@end
---
+++ test/test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by 赖永鹏 on 2018/12/17.
+//  Copyright © 2018年 LYP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
